af367fcb320b8ac216666c4d8b3a3b195378448e
Sabbir Hossain Shuvo <82939905+devlopersabbir@users.noreply.github.com>
2024-12-22 20:52:10 +0600

    [feat ✨] CRUD API 🎉🌋

--- src/main.ts
+++ src/main.ts
@@ -1,71 +1,70 @@
 import { Hono } from "hono";
-import type { Context } from "hono";
 import { logger } from "hono/logger";
+import type { Context } from "hono";
 import { db } from "./db.ts";
 import { posts } from "./schema.ts";
-import { eq, like } from "npm:drizzle-orm";
+import { eq, like } from "drizzle-orm";
 
 const app = new Hono();
 app.use(logger());
 
-app.get("/", (c: Context) => {
-  const id = c.req.param("id"); // as string
-  const query = c.req.query("query"); // as string
-
-  return c.text(`id: ${id}. query data ${query}`, {
-    status: 200,
-  });
-});
-
+// CRUD
 // create
 app.post("/posts", async (c: Context) => {
   const body = await c.req.json();
-  await db.insert(posts).values(body);
-
-  return c.text("post created");
+  try {
+    await db.insert(posts).values(body);
+    return c.json({ message: "Post created!" });
+  } catch (error) {
+    return c.json({ message: "Fail to create a post", error });
+  }
 });
-// read
+// get
 app.get("/posts", async (c: Context) => {
-  const post = await db.select().from(posts);
-  return c.json(post);
+  try {
+    const allPost = await db.select().from(posts);
+    if (!allPost.length) return c.json({ message: "No post found" });
+    return c.json(allPost);
+  } catch (error) {
+    return c.json({ message: "Fail to get posts", error });
+  }
 });
 // update
 app.put("/posts/:uuid", async (c: Context) => {
   const uuid = c.req.param("uuid");
-  const { name, description } = await c.req.json();
-  const pst = await db.query.posts.findFirst({
-    where: (post, { eq }) => eq(post.uuid, uuid),
-  });
-  if (!pst)
-    return c.json({
-      message: "Post not fund!",
-    });
+  const body = await c.req.json();
   try {
-    const updatePost = await db
-      .update(posts)
-      .set({
-        name: name || pst.name,
-        description: description || pst.description,
-      })
-      .where(eq(posts.uuid, uuid))
-      .returning();
-    return c.json(updatePost[0]);
+    const pst = await db.query.posts.findFirst({
+      where: (post, { eq }) => eq(post.uuid, uuid),
+    });
+    if (!pst) return c.json({ message: "Post not found" });
+    await db.update(posts).set(body).where(eq(posts.uuid, uuid));
+    return c.json({ message: "Post updated!" });
   } catch (error) {
-    console.log(error);
-    return c.text("something went wrong!");
+    return c.json({ message: "Fail to update post", error });
   }
 });
 // delete
 app.delete("/posts/:uuid", async (c: Context) => {
   const uuid = c.req.param("uuid");
-  await db.delete(posts).where(eq(posts.uuid, uuid)).returning();
-
-  return c.text("post deleted");
+  if (!uuid) return c.json({ message: "UUID not found" });
+  try {
+    const pst = await db.query.posts.findFirst({
+      where: (post, { eq }) => eq(post.uuid, uuid),
+    });
+    if (!pst) return c.json({ message: "Post not found" });
+    await db.delete(posts).where(eq(posts.uuid, uuid));
+    return c.json({ message: "Post deleted!" });
+  } catch (error) {
+    return c.json({ message: "Fail to delete post", error });
+  }
 });
-// find one by title
+// get single post
 app.get("/posts/search", async (c: Context) => {
   const query = c.req.query("q");
 
+  console.log("query: ", query);
+  if (!query) return c.json({ message: "Query not found" });
   const data = await db
     .select()
     .from(posts)
